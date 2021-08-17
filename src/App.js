c71297b50a42fb2ede74ed4e59df6f5e725373fb
chinmay <chinmay11.moghe@gmail.com>
2021-08-17 23:05:34 +0530

    setup changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <h1 className="company-name">Smart cows</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/task-1' component={Cards} />
+          <Route exact path='/task-2' component={BackgroundImage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
