fa19c4bd0f43e6dd37a2a1ae0c756dc140aa0543
chinmay <chinmay11.moghe@gmail.com>
2021-08-17 23:25:48 +0530

    routing changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.scss';
 import Home from './components/Home';
 import Cards from './components/Cards';
 import BackgroundImage from './components/BackgroundImage';
@@ -7,7 +6,6 @@ import BackgroundImage from './components/BackgroundImage';
 function App() {
   return (
     <div className="App">
-      <h1 className="company-name">Smart cows</h1>
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home} />
