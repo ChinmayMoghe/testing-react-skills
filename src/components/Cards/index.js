8fcd4ae8020439360055badf5d71bfb986ec843c
chinmay <chinmay11.moghe@gmail.com>
2021-08-18 16:48:30 +0530

    task 1 changes

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-
+import randomColor from 'randomcolor';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './style.module.scss';
 const Cards = () => {
+  const [word, setWord] = useState('');
+  const [navOpen, setnavOpen] = useState(false);
   return (
-    <div>Cards</div>
+    <div className={styles['container']}>
+      <nav className={styles['nav']}>
+        <Link className={styles['nav-link']} to='/home'>
+          Main Page
+        </Link>
+        <Link className={styles['nav-link']} to='/task-2'>
+          Task 2
+        </Link>
+      </nav>
+      <aside className={`${styles.sidebar} ${navOpen ? styles['open'] : styles['close']}`}>
+      </aside>
+      <main className={`${styles['main-section']} ${navOpen ? styles['open'] : styles['close']}`}>
+        <section className={styles['input-container']}>
+          <div className={styles.buttonContainer}><button className={`${styles.navButton} ${navOpen ? styles['open'] : styles['close']}`} type="button" onClick={() => setnavOpen(!navOpen)}></button></div>
+          <input value={word} onChange={(event) => setWord(event.target.value)} className={styles['text-input']} type='text' name='' id='' />
+          <div className={styles['cards-container']}>
+            {word && word.split('').map((letter, index) => <div key={btoa(letter + index)} style={{ backgroundColor: (index + 1) % 3 === 0 ? randomColor({}) : null }} className={styles['card']}>{letter}</div>)}
+          </div>
+        </section>
+      </main>
+    </div >
   );
 };
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './style.module.scss';
+import styles from './style.module.scss';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <header>
-      <h1 className='company-name'>Smart cows</h1>
-    </header>
+    <>
+      <header>
+        <h1 className={styles['company-name']}>Smart cows Assessment</h1>
+      </header>
+      <ul className={styles['task-list']}>
+        <li className={styles['task-list-item']}><Link to='/task-1'>Task 1</Link></li>
+        <li className={styles['task-list-item']}><Link to='/task-2'>Task 2</Link></li>
+      </ul>
+    </>
   );
 };
 
